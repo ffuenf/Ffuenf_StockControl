0e166e3ed6b14fd34ff972fa883e64fdd39aad39
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-04-11 18:52:13 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- app/code/local/Ffuenf/StockControl/Block/Adminhtml/Stockcontrol/Grid.php
+++ app/code/local/Ffuenf/StockControl/Block/Adminhtml/Stockcontrol/Grid.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Ffuenf_StockControl extension
- * 
- * NOTICE OF LICENSE
- * 
- * This source file is subject to the MIT License
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/mit-license.php
- * 
- * @category       Ffuenf
- * @package        Ffuenf_StockControl
- * @author         Achim Rosenhagen <a.rosenhagen@ffuenf.de>
- * @copyright      Copyright (c) 2015 ffuenf (http://www.ffuenf.de)
- * @license        http://opensource.org/licenses/mit-license.php MIT License
+     * Ffuenf_StockControl extension
+     * 
+     * NOTICE OF LICENSE
+     * 
+     * This source file is subject to the MIT License
+     * that is bundled with this package in the file LICENSE.txt.
+     * It is also available through the world-wide-web at this URL:
+     * http://opensource.org/licenses/mit-license.php
+     * 
+     * @category       Ffuenf
+     * @package        Ffuenf_StockControl
+     * @author         Achim Rosenhagen <a.rosenhagen@ffuenf.de>
+     * @copyright      Copyright (c) 2015 ffuenf (http://www.ffuenf.de)
+     * @license        http://opensource.org/licenses/mit-license.php MIT License
 */
 
 class Ffuenf_StockControl_Block_Adminhtml_Stockcontrol_Grid extends Mage_Adminhtml_Block_Widget_Grid
@@ -48,7 +48,7 @@ class Ffuenf_StockControl_Block_Adminhtml_Stockcontrol_Grid extends Mage_Adminht
             '{{table}}.stock_id=1',
             'left'
         );
-        $collection->addAttributeToFilter('type_id', array('in' => array('simple','downloadable','virtual')));
+        $collection->addAttributeToFilter('type_id', array('in' => array('simple', 'downloadable', 'virtual')));
         $this->setCollection($collection);
         return parent::_prepareCollection();
     }
