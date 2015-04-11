0961ca950ae3f01d88e33813416badbdce88b9d5
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-04-11 18:52:13 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- app/code/local/Ffuenf/StockControl/Block/Adminhtml/Stockcontrol/Grid/Renderer/Inventory.php
+++ app/code/local/Ffuenf/StockControl/Block/Adminhtml/Stockcontrol/Grid/Renderer/Inventory.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Ffuenf_StockControl extension
- * 
- * NOTICE OF LICENSE
- * 
- * This source file is subject to the MIT License
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/mit-license.php
- * 
- * @category       Ffuenf
- * @package        Ffuenf_StockControl
- * @author         Achim Rosenhagen <a.rosenhagen@ffuenf.de>
- * @copyright      Copyright (c) 2015 ffuenf (http://www.ffuenf.de)
- * @license        http://opensource.org/licenses/mit-license.php MIT License
+     * Ffuenf_StockControl extension
+     * 
+     * NOTICE OF LICENSE
+     * 
+     * This source file is subject to the MIT License
+     * that is bundled with this package in the file LICENSE.txt.
+     * It is also available through the world-wide-web at this URL:
+     * http://opensource.org/licenses/mit-license.php
+     * 
+     * @category       Ffuenf
+     * @package        Ffuenf_StockControl
+     * @author         Achim Rosenhagen <a.rosenhagen@ffuenf.de>
+     * @copyright      Copyright (c) 2015 ffuenf (http://www.ffuenf.de)
+     * @license        http://opensource.org/licenses/mit-license.php MIT License
 */
 
 class Ffuenf_StockControl_Block_Adminhtml_Stockcontrol_Grid_Renderer_Inventory extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
@@ -23,10 +23,10 @@ class Ffuenf_StockControl_Block_Adminhtml_Stockcontrol_Grid_Renderer_Inventory e
         $qty = $row->getData($this->getColumn()->getIndex());
         
         if (Mage::getStoreConfig('stockcontrol/general/inline_edit')) {
-            $html = '<input onblur="updateStock(this, '. $row->getId() .'); return false" class="validate-number input-text ' . $this->getColumn()->getInlineCss() . '" style="width:30%;float:left" type="text" name="qty" value="'.(float)$qty.'" />';
+            $html = '<input onblur="updateStock(this, ' . $row->getId() . '); return false" class="validate-number input-text ' . $this->getColumn()->getInlineCss() . '" style="width:30%;float:left" type="text" name="qty" value="' . (float) $qty . '" />';
         } else {
-            $html = '<input class="validate-number input-text ' . $this->getColumn()->getInlineCss() . '" style="width:30%;float:left" type="text" name="qty" value="'.(float)$qty.'" />';
-            $html .= '<button onclick="updateStock(this, '. $row->getId() .'); return false">' . Mage::helper('ffuenf_stockcontrol')->__('Update') . '</button>';
+            $html = '<input class="validate-number input-text ' . $this->getColumn()->getInlineCss() . '" style="width:30%;float:left" type="text" name="qty" value="' . (float) $qty . '" />';
+            $html .= '<button onclick="updateStock(this, ' . $row->getId() . '); return false">' . Mage::helper('ffuenf_stockcontrol')->__('Update') . '</button>';
         }
         return $html;
     }
